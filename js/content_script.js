6a4dfa13988af786b83ad7a78589481f6781e6ae
Robin Drexler <drexler.robin@gmail.com>
2014-10-21 07:53:19 +0200

    first working version with native form inputs only

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,8 @@
 $(function() {
-    alert('Yay, I\'m an extension :)');
+    $('body').bind('keydown', 'textarea, input', function(e) {
+      // pressed ENTER and either CMD/WIN or CTRL
+      if(e.which == 13 && (e.metaKey || e.ctrlKey)) {
+        $(e.target.form).submit();
+      }
+    });
 });
